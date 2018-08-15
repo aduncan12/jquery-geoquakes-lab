4bcff042e5c7eaf4ef7ed4c57d8be4ff2cb64347
sofia-aguirre <sofiaxaguirre@gmail.com>
2018-08-15 11:02:43 -0700

    began ajax

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,47 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+  $.ajax({
+    method: "GET",
+    url: endpoint,
+    success: onSuccess,
+    error: onError
+});
+
+function onSuccess(response) {
+  var quakeCount = response.metadata.count
+
+  for (var i = 0; i < response.features.length; i++) {
+  var titleQuake  = response.features[i].properties.title
+  var timeQuake = response.features[i].properties.time
+  var coordQuake = response.features[i].geometry.coordinates
+  $("#info").append(`<h1> ${titleQuake} </h1> <p> Happened on: ${timeQuake} </p>
+        <p>Location: ${coordQuake}</p>`);
+
+
+  
+  // var titleFirstQuake = response.features[0].properties.title
+  // var timeFirstQuake = response.features[0].properties.time
+  // var coordFirstQuake = response.features[0].geometry.coordinates
+
+// console.log(timeFirstQuake);
+  }
+}
+
+
+function onError(xhr, status, errorThrown) {
+  alert("Sorry, there was a problem!");
+  console.log("Error: " + errorThrown);
+  console.log("Status: " + status);
+  console.dir(xhr);
+}
+
+
+
 
 });
+
+
