ce5c1ecf82077dc990d923eed706c0ec80274e24
Alan <a386226@gmail.com>
2018-08-15 15:01:42 -0700

    maybe a map next time

--- scripts/app.js
+++ scripts/app.js
@@ -4,33 +4,39 @@ var endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_we
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
-  $.ajax({
+ 
+$.ajax({
     method: "GET",
     url: endpoint,
     success: onSuccess,
     error: onError
-});
+  });
+
 
 function onSuccess(response) {
   var quakeCount = response.metadata.count
+  var coordQuakeLat
+  var coordQuakeLong
 
   for (var i = 0; i < response.features.length; i++) {
   var titleQuake  = response.features[i].properties.title
   var timeQuake = response.features[i].properties.time
-  var coordQuake = response.features[i].geometry.coordinates
-  $("#info").append(`<h1> ${titleQuake} </h1> <p> Happened on: ${timeQuake} </p>
-        <p>Location: ${coordQuake}</p>`);
-
-
-  
-  // var titleFirstQuake = response.features[0].properties.title
-  // var timeFirstQuake = response.features[0].properties.time
-  // var coordFirstQuake = response.features[0].geometry.coordinates
-
-// console.log(timeFirstQuake);
+  coordQuakeLat = response.features[i].geometry.coordinates[0]
+  coordQuakeLong = response.features[i].geometry.coordinates[1]
+  $("#info").append(`<h4> ${titleQuake} </h4> <p> Happened on: ${timeQuake} </p>
+        <p>Longitude: ${coordQuakeLat} Latitude: ${coordQuakeLong}</p>`);
   }
-}
+      function initMap() {
+        // The location of the centerpoint
+        var quakeCenterpoint = {coordQuakeLat, coordQuakeLong};
+        // The map, centered at centerpoint
+        var map = new google.maps.Map(
+          $('#map').append(`<img src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg">`), {zoom: 4, center: quakeCenterpoint});
+        // The marker, positioned at quakeCenterpoint
+        var marker = new google.maps.Marker({position: quakeCenterpoint, map: map});
+      }
 
+}
 
 function onError(xhr, status, errorThrown) {
   alert("Sorry, there was a problem!");
@@ -40,8 +46,4 @@ function onError(xhr, status, errorThrown) {
 }
 
 
-
-
 });
-
-
